10c56c46797f0699dfb73fece35ffbe07fb0eea7
Iradium59 <samuel.definance003.jeu@gmail.com>
2024-07-02 12:25:24 +0200

    tp 2

---
+++ renderer.js
@@ -0,0 +1,17 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const { electron } = window;
+
+  function searchWeather() {
+    const city = document.getElementById('cityInput').value;
+    electron.send('searchWeather', city);
+  }
+
+  electron.receive('weatherData', (data) => {
+    const weatherInfoDiv = document.getElementById('weatherInfo');
+    weatherInfoDiv.innerHTML = `
+      <h2>${data.city}</h2><p>Température : ${data.temperature} °C</p><p>Humidité : ${data.humidity}%</p><p>Vitesse du vent : ${data.windSpeed} m/s</p>
+    `;
+  });
+  const searchButton = document.getElementById('searchButton');
+  searchButton.addEventListener('click', searchWeather);
+});
